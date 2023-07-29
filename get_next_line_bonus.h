2618199ba44baeaf36e8e5f8abf47469a9bc1c67
David Córdoba <133433382+dcordoban@users.noreply.github.com>
2023-07-29 13:26:17 +0200

    bonus added

---
+++ get_next_line_bonus.h
@@ -0,0 +1,32 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line_bonus.h                              :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: dcordoba <dcordoba@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/06/06 21:19:14 by dcordoba          #+#    #+#             */
+/*   Updated: 2023/07/29 12:58:06 by dcordoba         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_BONUS_H
+# define GET_NEXT_LINE_BONUS_H
+
+# ifndef BUFFER_SIZE
+#  define BUFFER_SIZE 10
+# endif
+
+# include <stdio.h>
+# include <stdlib.h>
+# include <unistd.h>
+# include <limits.h>
+
+char	*get_next_line(int fd);
+char	*ft_strjoin(char *s1, char *s2);
+int		ft_strchr(char *s, int c);
+char	*free_mem(char **buffer);
+char	*ft_substr(char *s, unsigned int start, size_t len);
+size_t	ft_strlen(const char	*s);
+
+#endif
